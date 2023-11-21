e1489a0a5ff55b963bc18aed20dcce415d7dea54
Raritetnik <mkodarenko@gmail.com>
2023-11-21 16:37:04 -0500

    test server side

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,9 +1,8 @@
 export default defineEventHandler((event) => {
-    return {success: true};
-    /*if (event.method === 'GET') {
+    if (event.method === 'GET') {
         return 'GETTED';
     }
     if (event.method === 'POST') {
         return 'POSTED';
-    }*/
+    }
 })
